943eed23a9d4c64a942d12ad159226f42d1d296a
kboss <atsugi.design@gmail.com>
2023-08-07 19:04:06 +0900

    投稿用タイムラインの作成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
+import Timeline from "@/components/Timeline";
 
 export default function Home() {
   return (
@@ -8,7 +9,10 @@ export default function Home() {
         <title>SNS Udemy</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+
+      <div>
+        <Timeline />
+      </div>
     </>
   );
 }
